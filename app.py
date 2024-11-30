2da61d08b968328500480779216d61ef0c8fefd0
CHAWALE ADITYA SUDHAKAR <adityachawale14@gmail.com>
2024-11-30 12:54:39 +0530

    Working

--- app.py
+++ app.py
@@ -8,46 +8,26 @@ import streamlit as st
 import random
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-ssl._create_default_https_context = ssl._create_unverified_context
-nltk.data.path.append(os.path.abspath("nltk_data"))
-nltk.download('punkt')
 
-# Load intents from the JSON file
-file_path = os.path.abspath("./intents.json")
-with open(file_path, "r") as file:
-    intents = json.load(file)
+import requests
+import google.generativeai as genai
 
-# Create the vectorizer and classifier
-vectorizer = TfidfVectorizer(ngram_range=(1,4))
-clf = LogisticRegression(random_state=0, max_iter=10000)
-
-# Preprocess the data
-tags = []
-patterns = []
-for intent in intents:
-    for pattern in intent['patterns']:
-        tags.append(intent['tag'])
-        patterns.append(pattern)
-
-# training the model
-x = vectorizer.fit_transform(patterns)
-y = tags
-clf.fit(x, y)
-
+# Set your Hugging Face API token
+HF_API_TOKEN = "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"  
 def chatbot(input_text):
-    input_text = vectorizer.transform([input_text])
-    tag = clf.predict(input_text)[0]
-    for intent in intents:
-        if intent['tag'] == tag:
-            response = random.choice(intent['responses'])
-            return response
-        
+    genai.configure(api_key="AIzaSyAJ9N35JKUcwycEKavtXDXVG_gNxodj2es")
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    response = model.generate_content(input_text)
+    return response.text
+   
 counter = 0
 
 def main():
     global counter
-    st.title("Intents of Chatbot using NLP")
+    st.title("Bhaiya Dj Chatbot")
 
     # Create a sidebar menu with options
     menu = ["Home", "Conversation History", "About"]
